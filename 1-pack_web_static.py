797efb8323d7b6f4ba9ca50eba269704ad203501
MKangabire <m.kangabire@alustudent.com>
2024-02-15 22:15:27 +0000

    deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
-"""decompresses and compresses"""
-
-from fabric.operations import local
+"""
+Write a Fabric script that generates
+a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo,
+using the function do_pack.
+"""
+import fabric.api as fab
 from datetime import datetime
 
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder."""
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_name = "web_static_{0}.tgz".format(timestamp)
-    archive_path = "versions/{0}".format(archive_name)
-
-    # Create the versions folder if it doesn't exist
-    local('mkdir -p versions')
-
-    # Compress the contents of the web_static folder into a .tgz archive
-    result = local("tar -cvzf {archive_path} web_static".format(archive_path))
-
-    # Check if the archive was generated successfully
-    if result.failed:
-        return None
-    else:
-        return archive_path
-
+    """
+    Prototype: def do_pack():
+    All files in the folder web_static must
+    be added to the final archive
+    All archives must be stored in the folder versions
+    (your function should create this folder if it doesn't exist)
+    The name of the archive created must be
+    web_static_<year><month><day><hour><minute><second>.tgz
+    The function do_pack must return the archive path
+    if the archive has been correctly generated.
+    Otherwise, it should return None
+    """
+    date_obj = datetime.now()
+    date_str = date_obj.isoformat()
+    char_rem = "-:.T"
+    count = 0
+    try:
+        while char_rem[count] in date_str:
+            date_str = date_str.split(char_rem[count])
+            date_str = "".join(date_str)
+            count += 1
+    except IndexError:
+        pass
+    fab.local("tar -czvf web_static_{0}.tgz web_static".format(date_str))
+    fab.local("mkdir -p ./versions/")
+    fab.local("mv *.tgz versions")
+    path = fab.local("cd ./versions/; readlink -f *.tgz", capture=True)
+    return path
