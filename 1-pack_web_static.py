528b544e47a9581cc339585e120ab66f7916301a
MKangabire <m.kangabire@alustudent.com>
2024-02-15 22:11:58 +0000

    deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """decompresses and compresses"""
 
-from fabric import task
-from fabric.context_managers import cd
 from fabric.operations import local
 from datetime import datetime
 
@@ -10,14 +8,14 @@ from datetime import datetime
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder."""
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_name = f'web_static_{timestamp}.tgz'
-    archive_path = f'versions/{archive_name}'
+    archive_name = "web_static_{0}.tgz".format(timestamp)
+    archive_path = "versions/{0}".format(archive_name)
 
     # Create the versions folder if it doesn't exist
     local('mkdir -p versions')
 
     # Compress the contents of the web_static folder into a .tgz archive
-    result = local(f'tar -cvzf {archive_path} web_static')
+    result = local("tar -cvzf {archive_path} web_static".format(archive_path))
 
     # Check if the archive was generated successfully
     if result.failed:
