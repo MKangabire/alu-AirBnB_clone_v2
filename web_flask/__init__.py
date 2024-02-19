22deae04e997462c41b07efe70c8243f34538782
MKangabire <m.kangabire@alustudent.com>
2024-02-19 03:21:56 -0800

    web flask

---
+++ web_flask/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""initializing file"""
+from flask import Flask
+
+def create_app():
+    # Create a Flask application instance
+    app = Flask(__name__)
+
+    # Import routes
+    from . import routes
+
+    # Register routes
+    app.register_blueprint(routes.bp)
+
+    return app
