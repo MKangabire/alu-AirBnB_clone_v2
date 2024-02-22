45dc42e3b1328742067f2000f5981b549cd9bd59
MKangabire <m.kangabire@alustudent.com>
2024-02-22 04:46:58 -0800

    çlone

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
-
-from flask import Flask, jsonify
+"""
+    Sript that starts a Flask web application
+"""
+from flask import Flask, render_template
 from models import storage
-
+import os
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Removes the current SQLAlchemy Session"""
+def handle_teardown(self):
+    """
+        method to handle teardown
+    """
     storage.close()
 
 
-@app.route('/cities_by_states', methods=['GET'])
-def cities_by_states():
-    """Returns a JSON list of all State objects"""
-    states = storage.all("State").values()
-    if not states:
-        return 'No states found', 500
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=sorted_states)
+@app.route('/cities_by_states', strict_slashes=False)
+def city_state_list():
+    """
+        method to render states from storage
+    """
+    states = storage.all('State').values()
+    return render_template("8-cities_by_states.html", states=states)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000)
