5b50124abb1f7a7bbcf13ef923d96fc19b8184ec
MKangabire <m.kangabire@alustudent.com>
2024-02-23 06:41:34 -0800

    flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -59,7 +59,7 @@ def var_num_even_odd(n):
         """
             function to display even or odd number
         """
-        number_type = "odd" if n % 2 != 0 else "even"
-        return render_template("6-number_odd_or_even.html", n=n, number_type=number_type)
+        num = "odd" if n % 2 != 0 else "even"
+        return render_template("6-number_odd_or_even.html", n=n, num=number)
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
