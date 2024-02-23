f00ed20992fd39b18b9f125bd29aaac679defad0
MKangabire <m.kangabire@alustudent.com>
2024-02-23 08:09:37 -0800

    flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,40 +1,48 @@
 #!/usr/bin/python3
-"""a script that starts a flask web"""
+"""
+    Sript that starts a Flask web application
+ """
 from flask import Flask
-from markupsafe import escape
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """initials the url"""
+def hello_hbn():
+    """
+        function to return Hello HBNB!
+    """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """
+        function to return HBNB
+    """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def text(text):
-    """texts"""
-    return 'C ' + escape(text).replace('_', ' ')
+def text_var(text):
+    """
+        function to display text variable passed in
+    """
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """text"""
-    return 'Python ' + escape(text).replace('_', ' ')
+def text_var_python(text="is cool"):
+    """
+        function to display text variable, with default "is cool"
+    """
+    return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """number"""
-    return f'{n} is a number'
-
+def var_num(n):
+        """
+             function to display a variable, but only if an int
+        """
+        return "{} is a number".format(n)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000)
